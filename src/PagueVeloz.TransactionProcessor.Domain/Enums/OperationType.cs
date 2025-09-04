8d1973cba8a4ac40d802ae3750ddbb3ba2ea0cf9
schimitegusta <gustavoschimitesoares0211@gmail.com>
2025-09-04 17:39:38 -0300

    build: estrutura inicial da solução TransactionProcessor

---
+++ src/PagueVeloz.TransactionProcessor.Domain/Enums/OperationType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagueVeloz.TransactionProcessor.Domain.Enums
+{
+    public enum OperationType
+    {
+        Credit,
+        Debit,
+        Reserve,
+        Capture,
+        Reversal,
+        Transfer
+    }
+}
---
+++ src/PagueVeloz.TransactionProcessor.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PagueVeloz.TransactionProcessor.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
